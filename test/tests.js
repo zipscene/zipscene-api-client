ebd239984c0967a07137a7d1ed278c6d85868a47
Andrew Hazlett <andrew.hazlett@zipscene.com>
2015-11-18 14:35:07 -0500

    style tests

--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,13 @@
-let expect = require('chai').expect;
+const expect = require('chai').expect;
 const XError = require('xerror');
-let JsonRPCApiClient = require('../lib/jsonrpc-api-client');
+const JsonRPCApiClient = require('../lib/jsonrpc-api-client');
 
-let DEFAULT_SERVER = 'http://localhost:3000';
-let DEFAULT_AUTH_SERVER = 'http://localhost:3001';
-let DEFAULT_USERNAME = 'admin';
-let DEFAULT_PASSWORD = '1234';
-let DEFAULT_ACCESS_TOKEN = 'accessToken1234';
-let DEFAULT_REFRESH_TOKEN = 'refreshToken1234';
+const DEFAULT_SERVER = 'http://localhost:3000';
+const DEFAULT_AUTH_SERVER = 'http://localhost:3001';
+const DEFAULT_USERNAME = 'admin';
+const DEFAULT_PASSWORD = '1234';
+const DEFAULT_ACCESS_TOKEN = 'accessToken1234';
+const DEFAULT_REFRESH_TOKEN = 'refreshToken1234';
 
 describe('JsonRPCApiClient', function() {
 
@@ -35,17 +35,14 @@ describe('JsonRPCApiClient', function() {
 		});
 
 		it('should throw an error if server is not set', function() {
-			expect( () => {
-				new JsonRPCApiClient({});
-			}).to.throw(XError.INVALID_ARGUMENT, 'Server must be set to make a request');
+			expect(() => new JsonRPCApiClient({}))
+				.to.throw(XError.INVALID_ARGUMENT, 'Server must be set to make a request');
 		});
 
 		it('should throw an error if no authentication is set', function() {
-			expect( () => {
-				new JsonRPCApiClient({ server: DEFAULT_SERVER });
-			}).to.throw(
-				XError.INVALID_ARGUMENT,
-				'Settings must set username and password or authToken or refreshToken');
+			let expectedMsg = 'Settings must set username and password or authToken or refreshToken';
+			expect(() => new JsonRPCApiClient({ server: DEFAULT_SERVER }))
+				.to.throw(XError.INVALID_ARGUMENT, expectedMsg);
 		});
 
 		it('should not throw an error when only auth is username and password', function() {
@@ -58,8 +55,8 @@ describe('JsonRPCApiClient', function() {
 			expect(client.server).to.equal(DEFAULT_SERVER);
 			expect(client.username).to.equal(DEFAULT_USERNAME);
 			expect(client.password).to.equal(DEFAULT_PASSWORD);
-			expect(client.accessToken).to.equal(undefined);
-			expect(client.refreshToken).to.equal(undefined);
+			expect(client.accessToken).to.be.undefined;
+			expect(client.refreshToken).to.be.undefined;
 			expect(client.authServer).to.equal(DEFAULT_SERVER);
 			expect(client.routeVersion).to.equal(2);
 		});
@@ -96,5 +93,3 @@ describe('JsonRPCApiClient', function() {
 	});
 
 });
-
-
