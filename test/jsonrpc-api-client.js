06b203daca044271a4e0ad920f5a53cb4e1ae5e7
Erin Thompson <erin.thompson@zipscene.com>
2015-12-22 10:10:46 -0500

    rearranging contructor tests so all undefined's are at the bottom and the checks are in the same order

--- test/jsonrpc-api-client.js
+++ test/jsonrpc-api-client.js
@@ -68,11 +68,11 @@ describe('JsonRPCApiClient', function() {
 				refreshToken: DEFAULT_REFRESH_TOKEN
 			});
 			expect(client.server).to.equal(DEFAULT_JSON_RPC_SERVER);
+			expect(client.authServer).to.equal(DEFAULT_AUTH_SERVER);
 			expect(client.username).to.equal(DEFAULT_USERNAME);
 			expect(client.password).to.equal(DEFAULT_PASSWORD);
 			expect(client.accessToken).to.equal(DEFAULT_ACCESS_TOKEN);
 			expect(client.refreshToken).to.equal(DEFAULT_REFRESH_TOKEN);
-			expect(client.authServer).to.equal(DEFAULT_AUTH_SERVER);
 			expect(client.routeVersion).to.equal(3);
 		});
 
@@ -90,12 +90,12 @@ describe('JsonRPCApiClient', function() {
 		it('should not throw an error when only auth is username and password', function() {
 			let client = new JsonRPCApiClient(DEFAULT_SETTINGS);
 			expect(client.server).to.equal(DEFAULT_JSON_RPC_SERVER);
+			expect(client.authServer).to.equal(DEFAULT_JSON_RPC_SERVER);
 			expect(client.username).to.equal(DEFAULT_USERNAME);
 			expect(client.password).to.equal(DEFAULT_PASSWORD);
+			expect(client.routeVersion).to.equal(2);
 			expect(client.accessToken).to.be.undefined;
 			expect(client.refreshToken).to.be.undefined;
-			expect(client.authServer).to.equal(DEFAULT_JSON_RPC_SERVER);
-			expect(client.routeVersion).to.equal(2);
 		});
 
 		it('should not throw an error when only accessToken is set', function() {
@@ -105,12 +105,12 @@ describe('JsonRPCApiClient', function() {
 			};
 			let client = new JsonRPCApiClient(settings);
 			expect(client.server).to.equal(DEFAULT_JSON_RPC_SERVER);
-			expect(client.username).to.equal(undefined);
-			expect(client.password).to.equal(undefined);
-			expect(client.accessToken).to.equal(DEFAULT_ACCESS_TOKEN);
-			expect(client.refreshToken).to.equal(undefined);
 			expect(client.authServer).to.equal(DEFAULT_JSON_RPC_SERVER);
+			expect(client.accessToken).to.equal(DEFAULT_ACCESS_TOKEN);
 			expect(client.routeVersion).to.equal(2);
+			expect(client.username).to.be.undefined;
+			expect(client.password).to.be.undefined;
+			expect(client.refreshToken).to.be.undefined;
 		});
 
 		it('should not throw an error when only refreshToken is set', function() {
@@ -120,12 +120,12 @@ describe('JsonRPCApiClient', function() {
 			};
 			let client = new JsonRPCApiClient(settings);
 			expect(client.server).to.equal(DEFAULT_JSON_RPC_SERVER);
-			expect(client.username).to.equal(undefined);
-			expect(client.password).to.equal(undefined);
-			expect(client.accessToken).to.equal(undefined);
-			expect(client.refreshToken).to.equal(DEFAULT_REFRESH_TOKEN);
 			expect(client.authServer).to.equal(DEFAULT_JSON_RPC_SERVER);
+			expect(client.refreshToken).to.equal(DEFAULT_REFRESH_TOKEN);
 			expect(client.routeVersion).to.equal(2);
+			expect(client.username).to.be.undefined;
+			expect(client.password).to.be.undefined;
+			expect(client.accessToken).to.undefined;
 		});
 	});
 
