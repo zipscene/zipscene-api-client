c803333424fe316bf20e16fc8c02b0a635a44054
Chris Breneman <crispy@cluenet.org>
2019-02-13 13:45:49 -0500

    ZipsceneApiClient -> ZipsceneRPCClient

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const { expect } = require('chai');
 const pasync = require('pasync');
 const MockAuth = require('./mock-auth');
 const MockDmp = require('./mock-dmp');
-const ZipsceneApiClient = require('../lib/zipscene-api-client');
+const ZipsceneRPCClient = require('../lib/zipscene-rpc-client');
 
 const DEFAULT_CONFIG = {
 	server: MockDmp.URL,
@@ -13,7 +13,7 @@ const DEFAULT_CONFIG = {
 	password: 'abc123'
 };
 
-describe('ZipsceneApiClient', () => {
+describe('ZipsceneRPCClient', () => {
 
 	// Set up mock auth service and dmp
 	let mockAuth, mockDmp;
@@ -33,11 +33,11 @@ describe('ZipsceneApiClient', () => {
 
 	it('should fail to create client with no credentials', () => {
 		let opts = objtools.merge({}, DEFAULT_CONFIG, { email: null, password: null });
-		expect(() => { return new ZipsceneApiClient(opts); }).to.throw(XError);
+		expect(() => { return new ZipsceneRPCClient(opts); }).to.throw(XError);
 	});
 
 	it('should authenticate with given credentials', () => {
-		let client = new ZipsceneApiClient(DEFAULT_CONFIG);
+		let client = new ZipsceneRPCClient(DEFAULT_CONFIG);
 		return client.request('check-token')
 			.then((result) => {
 				expect(result.success).to.equal(true);
@@ -45,7 +45,7 @@ describe('ZipsceneApiClient', () => {
 	});
 
 	it('should fail to authenticate with invalid credentials', () => {
-		let client = new ZipsceneApiClient(objtools.merge(
+		let client = new ZipsceneRPCClient(objtools.merge(
 			{},
 			DEFAULT_CONFIG,
 			{ password: MockAuth.BAD_PASSWORD }
@@ -60,7 +60,7 @@ describe('ZipsceneApiClient', () => {
 	});
 
 	it('should authenticate with given access token', () => {
-		let client = new ZipsceneApiClient(objtools.merge(
+		let client = new ZipsceneRPCClient(objtools.merge(
 			{},
 			DEFAULT_CONFIG,
 			{ email: null, password: null, accessToken: MockAuth.GOOD_TOKEN }
@@ -72,7 +72,7 @@ describe('ZipsceneApiClient', () => {
 	});
 
 	it('should fail to authenticate with bad provided access token', () => {
-		let client = new ZipsceneApiClient(objtools.merge(
+		let client = new ZipsceneRPCClient(objtools.merge(
 			{},
 			DEFAULT_CONFIG,
 			{ email: null, password: null, accessToken: '$junk$token' }
@@ -87,7 +87,7 @@ describe('ZipsceneApiClient', () => {
 	});
 
 	it('should rethrow token_expired error if access token was provided', () => {
-		let client = new ZipsceneApiClient(objtools.merge(
+		let client = new ZipsceneRPCClient(objtools.merge(
 			{},
 			DEFAULT_CONFIG,
 			{ email: null, password: null, accessToken: MockAuth.EXPIRED_TOKEN }
@@ -102,7 +102,7 @@ describe('ZipsceneApiClient', () => {
 	});
 
 	it('should allow client creation with no credentials if noAuth is set', () => {
-		let client = new ZipsceneApiClient(objtools.merge(
+		let client = new ZipsceneRPCClient(objtools.merge(
 			{},
 			DEFAULT_CONFIG,
 			{ email: null, password: null, noAuth: true }
@@ -115,7 +115,7 @@ describe('ZipsceneApiClient', () => {
 
 
 	it('should return the result of a basic request', () => {
-		let client = new ZipsceneApiClient(DEFAULT_CONFIG);
+		let client = new ZipsceneRPCClient(DEFAULT_CONFIG);
 		return client.request('times-two-not-three', { number: 8 })
 			.then((result) => {
 				expect(result.number).to.equal(16);
@@ -123,7 +123,7 @@ describe('ZipsceneApiClient', () => {
 	});
 
 	it('should throw the error returned by the remote server', () => {
-		let client = new ZipsceneApiClient(DEFAULT_CONFIG);
+		let client = new ZipsceneRPCClient(DEFAULT_CONFIG);
 		return client.request('times-two-not-three', { number: 3 })
 			.then((result) => {
 				throw new Error('Shouldnt be here');
@@ -134,7 +134,7 @@ describe('ZipsceneApiClient', () => {
 	});
 
 	it('should support automatic retries', () => {
-		let client = new ZipsceneApiClient(DEFAULT_CONFIG);
+		let client = new ZipsceneRPCClient(DEFAULT_CONFIG);
 		return client.request('toggle', {}, { maxRetries: 2 })
 			.catch(() => {
 				throw new Error('Shouldnt be here');
@@ -153,7 +153,7 @@ describe('ZipsceneApiClient', () => {
 	});
 
 	it('should re-authenticate if access token is expired', () => {
-		let client = new ZipsceneApiClient(DEFAULT_CONFIG);
+		let client = new ZipsceneRPCClient(DEFAULT_CONFIG);
 		return client.request('pass')
 			.then(() => {
 				// Stub in expired token
@@ -167,7 +167,7 @@ describe('ZipsceneApiClient', () => {
 
 
 	it('should consume a streaming response', () => {
-		let client = new ZipsceneApiClient(DEFAULT_CONFIG);
+		let client = new ZipsceneRPCClient(DEFAULT_CONFIG);
 		let results = [];
 		return client.requestStream('stream')
 			.through((data) => {
@@ -180,7 +180,7 @@ describe('ZipsceneApiClient', () => {
 	});
 
 	it('should fail if stream hangs up with a success object', () => {
-		let client = new ZipsceneApiClient(DEFAULT_CONFIG);
+		let client = new ZipsceneRPCClient(DEFAULT_CONFIG);
 		let results = [];
 		return client.requestStream('stream', { truncate: true })
 			.through((data) => {
@@ -196,7 +196,7 @@ describe('ZipsceneApiClient', () => {
 	});
 
 	it('should throw an error returned within a streaming response', () => {
-		let client = new ZipsceneApiClient(DEFAULT_CONFIG);
+		let client = new ZipsceneRPCClient(DEFAULT_CONFIG);
 		let results = [];
 		return client.requestStream('stream-error')
 			.through((data) => {
@@ -212,7 +212,7 @@ describe('ZipsceneApiClient', () => {
 	});
 
 	it('should reauthenticate if access token is expired during streaming request', () => {
-		let client = new ZipsceneApiClient(DEFAULT_CONFIG);
+		let client = new ZipsceneRPCClient(DEFAULT_CONFIG);
 		let results = [];
 		return client.request('pass')
 			.then(() => {
